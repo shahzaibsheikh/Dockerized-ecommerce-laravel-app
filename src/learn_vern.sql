eb0dcadf9799b76c2f987ec639fe7a394a0f9d42
shahzaib khurshid <48204885+shahzaibsheikh@users.noreply.github.com>
2025-07-20 20:05:29 +0500

    DB

---
+++ src/learn_vern.sql
@@ -0,0 +1,227 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 20, 2025 at 04:53 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `learn_vern`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+-- Error reading structure for table learn_vern.brands: #1932 - Table &#039;learn_vern.brands&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.brands: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`brands`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carts`
+--
+-- Error reading structure for table learn_vern.carts: #1932 - Table &#039;learn_vern.carts&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.carts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`carts`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cities`
+--
+-- Error reading structure for table learn_vern.cities: #1932 - Table &#039;learn_vern.cities&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.cities: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`cities`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+-- Error reading structure for table learn_vern.comments: #1932 - Table &#039;learn_vern.comments&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.comments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`comments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `countries`
+--
+-- Error reading structure for table learn_vern.countries: #1932 - Table &#039;learn_vern.countries&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.countries: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`countries`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `crud_operations`
+--
+-- Error reading structure for table learn_vern.crud_operations: #1932 - Table &#039;learn_vern.crud_operations&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.crud_operations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`crud_operations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+-- Error reading structure for table learn_vern.customers: #1932 - Table &#039;learn_vern.customers&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.customers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`customers`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dealers`
+--
+-- Error reading structure for table learn_vern.dealers: #1932 - Table &#039;learn_vern.dealers&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.dealers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`dealers`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exception_logs`
+--
+-- Error reading structure for table learn_vern.exception_logs: #1932 - Table &#039;learn_vern.exception_logs&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.exception_logs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`exception_logs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+-- Error reading structure for table learn_vern.failed_jobs: #1932 - Table &#039;learn_vern.failed_jobs&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`failed_jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+-- Error reading structure for table learn_vern.jobs: #1932 - Table &#039;learn_vern.jobs&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `line_items`
+--
+-- Error reading structure for table learn_vern.line_items: #1932 - Table &#039;learn_vern.line_items&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.line_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`line_items`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table learn_vern.migrations: #1932 - Table &#039;learn_vern.migrations&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+-- Error reading structure for table learn_vern.orders: #1932 - Table &#039;learn_vern.orders&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.orders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`orders`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset`
+--
+-- Error reading structure for table learn_vern.password_reset: #1932 - Table &#039;learn_vern.password_reset&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.password_reset: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`password_reset`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+-- Error reading structure for table learn_vern.password_resets: #1932 - Table &#039;learn_vern.password_resets&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.password_resets: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`password_resets`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+-- Error reading structure for table learn_vern.personal_access_tokens: #1932 - Table &#039;learn_vern.personal_access_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.personal_access_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`personal_access_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+-- Error reading structure for table learn_vern.products: #1932 - Table &#039;learn_vern.products&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.products: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`products`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `states`
+--
+-- Error reading structure for table learn_vern.states: #1932 - Table &#039;learn_vern.states&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.states: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`states`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+-- Error reading structure for table learn_vern.students: #1932 - Table &#039;learn_vern.students&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.students: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`students`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `telescope_entries`
+--
+-- Error reading structure for table learn_vern.telescope_entries: #1932 - Table &#039;learn_vern.telescope_entries&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.telescope_entries: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`telescope_entries`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `telescope_entries_tags`
+--
+-- Error reading structure for table learn_vern.telescope_entries_tags: #1932 - Table &#039;learn_vern.telescope_entries_tags&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.telescope_entries_tags: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`telescope_entries_tags`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `telescope_monitoring`
+--
+-- Error reading structure for table learn_vern.telescope_monitoring: #1932 - Table &#039;learn_vern.telescope_monitoring&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.telescope_monitoring: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`telescope_monitoring`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `translation`
+--
+-- Error reading structure for table learn_vern.translation: #1932 - Table &#039;learn_vern.translation&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.translation: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`translation`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table learn_vern.users: #1932 - Table &#039;learn_vern.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table learn_vern.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `learn_vern`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
